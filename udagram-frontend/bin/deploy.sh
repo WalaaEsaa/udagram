7a0f9db76c86a0e71f20b4eae68556d576865905
walaa esaa <40539477+WalaaEsaa@users.noreply.github.com>
2022-10-04 22:53:18 +0200

    Update deploy.sh

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://my-udagram-frontend/
-eb init && eb use http://udagramapi-env.eba-tjs8r4ge.us-east-1.elasticbeanstalk.com/ && eb deploy && eb setenv PORT=$port PASSWORD=$password
+
